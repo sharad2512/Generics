068be691d8cea476e49fe17122035e4255f4afed
sharad2512 <shnarute95@gmail.com>
2023-01-30 09:32:09 +0530

    ADDED : code to find max number from three integer

--- src/main/java/com/bridelabz/generics/FindMaxInteger.java
+++ src/main/java/com/bridelabz/generics/FindMaxInteger.java
@@ -1,6 +1,17 @@
 package com.bridelabz.generics;
 public class FindMaxInteger {
+    public static Integer compareTo(Integer a,Integer b,Integer c)
+    {
+        if (a > b && a > c) {
+            return a;
+        } else if (b > c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Generics Program");
+        System.out.println("Welcome to find maximum no from three integer");
+        System.out.println("Max Integer Nmber is: "+FindMaxInteger.compareTo(15, 32, 45));
     }
 }
